86b8e256a7a52f38798c7e551a6cd0eed56978ee
Masih-Saldanha <masih.saldanha@gmail.com>
2022-04-28 10:19:24 -0300

    Adicionados dotenv e mongodb

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,17 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
-const PORTA = 5000;
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+// console.log(mongoClient);
+let db;
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(json());
 
-app.listen(PORTA, () => {
-    console.log(`Servidor ligado na porta ${PORTA}`);
+app.listen(process.env.PORTA, () => {
+    console.log(`Servidor ligado na porta ${process.env.PORTA}`);
 });
